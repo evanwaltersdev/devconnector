382463dc693719eae37aeabcaecb42b0b84994bc
Evan Walters <evan@evanw.uk>
2020-03-08 11:02:31 +0000

    Added user validation

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB()
 
 app.get('/', (req, res) => res.send('API running'))
 
+// Init middleware
+app.use(
+  express.json({
+    extended: false
+  })
+)
+
 // Define routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
